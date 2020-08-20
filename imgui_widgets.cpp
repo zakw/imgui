fad943ff654e7b81324dc2d78b777584c05299bd
ocornut <omarcornut@gmail.com>
2020-08-20 20:46:57 +0200

    RangeSelect/MultiSelect: Temporary fix/work-around for child/popup to not inherit MultiSelectEnabled flag, until we make mulit-select data stackable.

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -5695,7 +5695,7 @@ bool ImGui::TreeNodeBehavior(ImGuiID id, ImGuiTreeNodeFlags flags, const char* l
     const bool was_selected = selected;
 
     // Multi-selection support (header)
-    const bool is_multi_select = g.MultiSelectEnabled;
+    const bool is_multi_select = (g.MultiSelectEnabledWindow == window);
     if (is_multi_select)
     {
         MultiSelectItemHeader(id, &selected);
@@ -6043,7 +6043,7 @@ bool ImGui::Selectable(const char* label, bool selected, ImGuiSelectableFlags fl
     const bool was_selected = selected;
 
     // Multi-selection support (header)
-    const bool is_multi_select = g.MultiSelectEnabled;
+    const bool is_multi_select = (g.MultiSelectEnabledWindow == window);
     if (is_multi_select)
     {
         MultiSelectItemHeader(id, &selected);
@@ -6144,7 +6144,7 @@ ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void*
     ImGuiContext& g = *GImGui;
     ImGuiWindow* window = g.CurrentWindow;
 
-    IM_ASSERT(g.MultiSelectEnabled == false);   // No recursion allowed yet (we could allow it if we deem it useful)
+    IM_ASSERT(g.MultiSelectEnabledWindow == NULL);   // No recursion allowed yet (we could allow it if we deem it useful)
     IM_ASSERT(g.MultiSelectFlags == 0);
     IM_ASSERT(g.MultiSelectState.FocusScopeId == 0);    
 
@@ -6153,7 +6153,7 @@ ImGuiMultiSelectData* ImGui::BeginMultiSelect(ImGuiMultiSelectFlags flags, void*
     state->Clear();
     state->BackupFocusScopeId = window->DC.NavFocusScopeIdCurrent;
     state->FocusScopeId = window->DC.NavFocusScopeIdCurrent = window->IDStack.back();
-    g.MultiSelectEnabled = true;
+    g.MultiSelectEnabledWindow = window;
     g.MultiSelectFlags = flags;
 
     // Use copy of keyboard mods at the time of the request, otherwise we would requires mods to be held for an extra frame.
@@ -6203,7 +6203,7 @@ ImGuiMultiSelectData* ImGui::EndMultiSelect()
         state->Out.RangeValue = true;
     g.MultiSelectState.FocusScopeId = 0;
     window->DC.NavFocusScopeIdCurrent = g.MultiSelectState.BackupFocusScopeId;
-    g.MultiSelectEnabled = false;
+    g.MultiSelectEnabledWindow = NULL;
     g.MultiSelectFlags = ImGuiMultiSelectFlags_None;
 
 #ifdef IMGUI_DEBUG_MULTISELECT
